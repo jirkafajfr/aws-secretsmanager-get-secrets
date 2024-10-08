a5234e99891ec85ce1fb2ed2472e57c285a3ad5a
Jirka Fajfr <fajfr@amazon.com>
2024-10-08 18:35:43 -0500

    Test 3

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import * as core from '@actions/core'
+import * as core from '@actions/core';
+import { setDefaultAutoSelectFamilyAttemptTimeout } from 'net';
 import { SecretsManagerClient } from "@aws-sdk/client-secrets-manager";
 import {
     buildSecretsList,
@@ -13,6 +14,8 @@ import { CLEANUP_NAME } from "./constants";
 
 export async function run(): Promise<void> {
     try {
+        setDefaultAutoSelectFamilyAttemptTimeout(1000);
+
         // Default client region is set by configure-aws-credentials
         const client : SecretsManagerClient = new SecretsManagerClient({region: process.env.AWS_DEFAULT_REGION, customUserAgent: "github-action"});
         const secretConfigInputs: string[] = [...new Set(core.getMultilineInput('secret-ids'))];
